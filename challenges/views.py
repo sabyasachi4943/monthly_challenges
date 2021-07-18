3f3115af0aaf2d2ef34ee68fb1ceb8c2b69a4c04
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-07-18 11:44:17 +0600

    urls & views

---
+++ challenges/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+
+monthly_challenges = {
+    "january" : "Eat no meat for the entire month!",
+    "february" : "Walk for at least 20 minutes every day!",
+    "march" : "Learn Django at least 20 minutes every day!",
+    "april" : "do something in april",
+    "may" : "do something in may",
+    "june" : "do something in june",
+    "july" : "do something in july",
+    "august" : "do something in august",
+    "september" : "do something in september",
+    "october" : "do something in october",
+    "november" : "do something in november",
+    "december" : "shes"
+
+}
+# Create your views here.
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("not valid")
+
+    return HttpResponse(challenge_text)
