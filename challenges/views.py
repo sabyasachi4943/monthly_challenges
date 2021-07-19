f22a0c81d6c2982b062a61fd2d8f03062a903ead
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-07-19 14:58:19 +0600

    added 404 page

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -57,4 +57,5 @@ def monthly_challenge(request, month):
             "month_name": month,
         })
     except:
-        return HttpResponseNotFound("not valid")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
